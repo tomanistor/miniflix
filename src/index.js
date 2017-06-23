589421e9d86ef73dedb125c9d1173753db1a8432
tomanistor <toma.nistor@gmail.com>
2017-06-23 13:18:03 -0700

    Added components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Upload from './components/Upload';
+import Display from './components/Display';
+import Callback from './components/Callback';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { Router, Route, browserHistory } from 'react-router';
+import { requireAuth } from './utils/AuthService';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={Display}/>
+        <Route path="/upload" component={Upload} onEnter={requireAuth} />
+        <Route path="/callback" component={Callback} />
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
