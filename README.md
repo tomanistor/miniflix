48e72b36e6c7f6b7976054cdb67bb246eb77b36e
tomanistor <toma.nistor@gmail.com>
2017-08-07 01:12:26 -0700

    Added logo, favicon, and restyled navbar

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Miniflix
 A small app for uploading, viewing, and sharing videos.
+* [miniflix](https://miniflix-react.herokuapp.com)
 
 # Tools
 * [create-react-app](https://github.com/facebookincubator/create-react-app)
-* auth0-js
+* [auth0-js](https://github.com/auth0/auth0.js)
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,32 +2,35 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { login, logout, isLoggedIn } from '../utils/AuthService';
 import '../App.css';
+import logo from '../logo.jpg';
 
 class Nav extends Component {
 
   render() {
     return (
       <nav className="navbar navbar-default">
-        <div className="navbar-header">
-          <Link className="navbar-brand" to="/">Miniflix</Link>
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/"><img src={logo} alt="logo" /></Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">All Videos</Link>
+            </li>
+            <li>
+              {
+               ( isLoggedIn() ) ? <Link to="/upload">Upload Videos</Link> :  ''
+              }
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+             {
+               (isLoggedIn()) ? ( <button className="btn btn-danger log" onClick={() => logout()}>Log out</button> ) : ( <button className="btn btn-info log" onClick={() => login()}>Log In</button> )
+             }
+            </li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/">All Videos</Link>
-          </li>
-          <li>
-            {
-             ( isLoggedIn() ) ? <Link to="/upload">Upload Videos</Link> :  ''
-            }
-          </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-           {
-             (isLoggedIn()) ? ( <button className="btn btn-danger log" onClick={() => logout()}>Log out</button> ) : ( <button className="btn btn-info log" onClick={() => login()}>Log In</button> )
-           }
-          </li>
-        </ul>
       </nav>
     );
   }
